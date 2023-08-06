0d00dade3ef9c765099db7e17a8da50f2619dd63
Corneliu Popeea <popeea@cqse.eu>
2023-08-06 14:44:49 +0200

    eureka-server

---
+++ eureka-server/src/test/java/com/greglturnquist/learningspringboot/learningspringbooteurekaserver/LearningSpringBootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.learningspringboot.learningspringbooteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
